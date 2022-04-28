591d29401c406c4f63b456da74e1dac2dac09c52
soraef <sorasorasorayama@gmail.com>
2022-04-28 17:11:55 +0900

    update comment

--- lib/entity_map.dart
+++ lib/entity_map.dart
@@ -5,37 +5,56 @@
 import 'entity.dart';
 import 'package:collection/collection.dart';
 
+/// The [EntityMap] class holds multiple Entities and provides Immutable [Entity] collection operations.
+///
+/// Entity<Id> is a class with an id property of type Id.
+/// [EntityMap] holds generic [Id]-[Entity] pairs.
+///
+/// [EntityMap] class internally holds [Entity] as Map<Id, Entity>,
+/// so [Entity] does not allow duplicate [Id]
 class EntityMap<Id, E extends Entity<Id>> {
   final Map<Id, E> _entities;
 
   const EntityMap(this._entities);
 
+  /// Create an empty [EntityMap]
   factory EntityMap.empty() => EntityMap<Id, E>(const {});
 
+  /// Creates an [EntityMap] from an Iterable<Entity>.
   factory EntityMap.fromIterable(Iterable<E> entities) {
     return EntityMap<Id, E>({for (final entity in entities) entity.id: entity});
   }
 
+  /// Returns all Entities held by [this].
   Iterable<E> get entities => _entities.values;
 
+  /// Returns all the Id's that [this] holds
   Iterable<Id> get ids => _entities.keys;
 
+  /// Returns the number of Entities that [this] holds.
   int get length => entities.length;
 
+  /// returns true if this has an [id]
   bool exist(Id id) => _entities[id] != null;
 
+  /// Returns the [Entity] of the [id] that this holds
   E? byId(Id id) => _entities[id];
 
-  EntityMap<Id, E> byIds(List<Id> ids) {
+  /// Returns the [Entity] of [ids] held by this as a new [EntityMap].
+  EntityMap<Id, E> byIds(Iterable<Id> ids) {
     return EntityMap.fromIterable(
       ids.where((id) => exist(id)).map((id) => byId(id)!),
     );
   }
 
+  /// Returns a new [EntityMap] with additional [entity].
+  /// If the [entity] already exists, it is overwritten.
   EntityMap<Id, E> put(E entity) {
     return EntityMap<Id, E>({..._entities, entity.id: entity});
   }
 
+  /// Returns a new [EntityMap] with additional [entities].
+  /// Existing [Entity] objects are overwritten.
   EntityMap<Id, E> putAll(Iterable<E> entities) {
     return EntityMap<Id, E>({
       ..._entities,
@@ -43,12 +62,14 @@ class EntityMap<Id, E extends Entity<Id>> {
     });
   }
 
+  /// Returns a new [EntityMap] with the [entity] removed.
   EntityMap<Id, E> remove(E entity) {
     return EntityMap<Id, E>(
       {..._entities}..removeWhere((id, _) => id == entity.id),
     );
   }
 
+  /// Returns a new [EntityMap] with the [entities] removed.
   EntityMap<Id, E> removeAll(Iterable<E> entities) {
     return EntityMap<Id, E>(
       {..._entities}..removeWhere(
@@ -57,30 +78,38 @@ class EntityMap<Id, E extends Entity<Id>> {
     );
   }
 
+  /// Returns a new [EntityMap] with all Entities removed that satisfy the given [test].
   EntityMap<Id, E> removeWhere(bool Function(E entity) test) {
     return EntityMap<Id, E>(
       {..._entities}..removeWhere((id, e) => test(e)),
     );
   }
 
+  /// Returns a new [EntityMap] with the Entity with [id] removed.
   EntityMap<Id, E> removeById(Id id) {
     return EntityMap<Id, E>(
       {..._entities}..removeWhere((_id, entity) => id == _id),
     );
   }
 
+  /// Returns a new [EntityMap] with [other] merged.
   EntityMap<Id, E> marge(EntityMap<Id, E> other) {
     return EntityMap<Id, E>({..._entities, ...other._entities});
   }
 
+  /// Returns a new [EntityMap] that holds all Entities that satisfy the given [test].
   EntityMap<Id, E> where(bool Function(E) test) {
     return EntityMap.fromIterable(_entities.values.where(test));
   }
 
+  /// The current elements of this iterable modified by convert.
   Iterable<T> map<T>(T Function(E entity) convert) {
     return entities.map(convert);
   }
 
+  /// Creates a sorted list of the elements of the iterable.
+  ///
+  /// The elements are ordered by the [compare] [Comparator].
   List<E> sorted(int Function(E a, E b) compare) {
     return entities.sorted(compare);
   }
--- test/entity_collection_test.dart
+++ test/entity_collection_test.dart
@@ -1,5 +1,98 @@
+import 'package:entity_collection/entity_collection.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+class Person implements Entity<String> {
+  @override
+  final String id;
+  final String name;
+  final int age;
+
+  Person({
+    required this.id,
+    required this.name,
+    required this.age,
+  });
+}
+
+typedef PersonMap = EntityMap<String, Person>;
+
 void main() {
-  test('', () {});
+  final person1 = Person(id: "1", name: "Hoge", age: 1);
+  final person2 = Person(id: "2", name: "Fuga", age: 2);
+  final person3 = Person(id: "3", name: "Sora", age: 3);
+
+  test('exist', () {
+    final personMap = PersonMap.fromIterable([person1, person2]);
+    expect(personMap.exist(person1.id), true);
+    expect(personMap.exist(person3.id), false);
+  });
+
+  test('byId', () {
+    final personMap = PersonMap.fromIterable([person1, person2]);
+    final person = personMap.byId(person1.id);
+    final notExist = personMap.byId(person3.id);
+    expect(person, person1);
+    expect(notExist, null);
+  });
+
+  test('put', () {
+    final personMap = PersonMap.empty();
+    final putPersonMap = personMap.put(person1);
+    expect(personMap != putPersonMap, true);
+    expect(personMap.length, 0);
+    expect(putPersonMap.length, 1);
+  });
+
+  test('putAll', () {
+    final personMap = PersonMap.empty();
+    final putPersonMap = personMap.putAll([person1, person2]);
+    expect(personMap != putPersonMap, true);
+    expect(personMap.length, 0);
+    expect(putPersonMap.length, 2);
+  });
+
+  test('remove', () {
+    final personMap = PersonMap.fromIterable([person1, person2]);
+    final removePersonMap = personMap.remove(person1);
+    expect(personMap != removePersonMap, true);
+    expect(personMap.length, 2);
+    expect(removePersonMap.length, 1);
+  });
+
+  test('removeAll', () {
+    final personMap = PersonMap.fromIterable([person1, person2, person3]);
+    final removePersonMap = personMap.removeAll([person1, person2]);
+    expect(personMap != removePersonMap, true);
+    expect(personMap.length, 3);
+    expect(removePersonMap.length, 1);
+  });
+
+  test('removeById', () {
+    final personMap = PersonMap.fromIterable([person1, person2, person3]);
+    final removePersonMap = personMap.removeById(person1.id);
+    expect(personMap != removePersonMap, true);
+    expect(personMap.length, 3);
+    expect(removePersonMap.length, 2);
+  });
+  test('merge', () {
+    final personMap1 = PersonMap.fromIterable([person1, person2]);
+    final personMap2 = PersonMap.fromIterable([person2, person3]);
+    final mergedMap = personMap1.marge(personMap2);
+    expect(mergedMap != personMap1, true);
+    expect(mergedMap != personMap2, true);
+    expect(mergedMap.length, 3);
+  });
+
+  test('where', () {
+    final personMap = PersonMap.fromIterable([person1, person2, person3]);
+    // expect(personMap.where((p0) => p0.age))
+  });
+
+  test('map', () {
+    final personMap = PersonMap.fromIterable([person1, person2, person3]);
+  });
+
+  test('sort', () {
+    final personMap = PersonMap.fromIterable([person1, person2, person3]);
+  });
 }
